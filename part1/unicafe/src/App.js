64edcfde04491ca32f6a09d7b4054bf874a93543
dtraid <diego@dtraid.com>
2021-06-07 09:52:49 +0200

    Exercise 1.6: unicafe step1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,7 +6,24 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button text="good" handleClick={() => setGood(good + 1)} />
+      <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
+      <Button text="bad" handleClick={() => setBad(bad + 1)} />
+      <h1>Statistics</h1>
+      good {good}
+      <br />
+      neutral {neutral}
+      <br />
+      bad {bad}
+    </div>
+  );
 };
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
 export default App;
