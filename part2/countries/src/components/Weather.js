b3265ac8d1c737a040adce7467f0a4f8e75933b1
dtraid <diego@dtraid.com>
2021-06-12 21:37:00 +0200

    2.14*: Data for countries, step3

--- part2/countries/src/components/Weather.js
+++ part2/countries/src/components/Weather.js
@@ -2,37 +2,34 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Weather = ({ city }) => {
-  const [currentWeather, setWeather] = useState({});
+  const [weather, setWeather] = useState();
   const apiKey = process.env.REACT_APP_API_KEY;
   const queryUrl = `http://api.weatherstack.com/current?access_key=${apiKey}&query=${city}`;
 
-  useEffect(() =>
-    axios.get(queryUrl).then((res) => setWeather(res.data.current), [])
-  );
+  useEffect(() => axios.get(queryUrl).then((res) => setWeather(res.data), []));
+
+  if (!weather) return <div>Loading weather...</div>;
+  if (!weather.success) {
+    return <div>Error fetching weather: {weather.error.info}</div>;
+  }
 
   return (
     <div>
-      {currentWeather ? (
-        <>
-          <h2>Weather in {city}</h2>
-          <div>
-            <strong>temperature:</strong> {currentWeather.temperature} Celsius
-          </div>
-          {currentWeather.weather_icons.map((icon, index) => (
-            <img
-              alt={currentWeather.weather_descriptions[index]}
-              key={currentWeather.weather_descriptions[index]}
-              src={icon}
-            />
-          ))}
-          <div>
-            <strong>wind:</strong> {currentWeather.wind_speed} mph direction{' '}
-            {currentWeather.wind_dir}
-          </div>
-        </>
-      ) : (
-        'Loading weather...'
-      )}
+      <h2>Weather in {city}</h2>
+      <div>
+        <strong>temperature:</strong> {weather.temperature} Celsius
+      </div>
+      {weather.weather_icons.map((icon, index) => (
+        <img
+          alt={weather.weather_descriptions[index]}
+          key={weather.weather_descriptions[index]}
+          src={icon}
+        />
+      ))}
+      <div>
+        <strong>wind:</strong> {weather.wind_speed} mph direction{' '}
+        {weather.wind_dir}
+      </div>
     </div>
   );
 };
