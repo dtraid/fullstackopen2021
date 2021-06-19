c198a16be1037b97c40f1ba153960f01e5eb629e
dtraid <diego@dtraid.com>
2021-06-19 20:33:02 +0200

    Exercise 3.21 Deploying the database backend to production

--- part3/phonebook/src/services/persons.js
+++ part3/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'https://superphonebook2turbo.herokuapp.com/api/persons';
+const baseUrl = '/api/persons';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
